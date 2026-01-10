bd968c7b0c1c4d8386e141858edcfec6ed053eaf
Oliver Mustoe <polivermustoe@gmail.com>
2026-01-10 02:11:01 -0500

    caddy is easy and I just didnt understand ball

--- setups_n_cleanups/server_cleanup.sh
+++ setups_n_cleanups/server_cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Cleanup all files on server for quick testing, I scp all of the files over so I cleanup my files (rsync in future)
-cd ~
-rm -rf poc-GeneralNetworkAutomation
+cd ~/poc-GeneralNetworkAutomation
+sudo docker compose down
 rm -rf /opt/forgejo
+cd ~
+rm -rf ./poc-GeneralNetworkAutomation
