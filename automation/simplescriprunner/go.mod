8d5c091d573cd0c8e87a0810b76d4c40bf757860
Oliver Mustoe <polivermustoe@gmail.com>
2026-02-07 17:53:16 -0500

    We can connect gamers

--- automation/simplescriprunner/go.mod
+++ automation/simplescriprunner/go.mod
@@ -2,4 +2,8 @@ module om/simplescriptrunner/v2
 
 go 1.25.5
 
-require github.com/goccy/go-yaml v1.19.2 // indirect
+require (
+	github.com/goccy/go-yaml v1.19.2 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+)
--- automation/simplescriprunner/simplescriptrunner.go
+++ automation/simplescriprunner/simplescriptrunner.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/goccy/go-yaml"
+	"golang.org/x/crypto/ssh"
+	"io"
 	"log"
 	"os"
 )
@@ -24,21 +26,80 @@ func check(errormsg error) {
 func main() {
 	// Grab flags
 	var fileArg string
-	// Temp default value of the nokia file
+	var hostArg string
+	var portArg string
+	// Temp default value of the nokia file and 192.168.0.1
 	flag.StringVar(&fileArg, "file", "nokia.yml", "File that contains login and a hello msg")
+	flag.StringVar(&hostArg, "host", "192.168.0.1", "Host you want to target")
+	flag.StringVar(&portArg, "port", "22", "Port on your host you want to target")
 	// Parse flags
 	flag.Parse()
 
 	// Open and check file contents
 	fileContentsPtr, err := os.ReadFile(fileArg)
 	check(err)
-	//defer fileContentsPtr.Close()
 
 	// Read contents into YAML parser -
 	var yamlContents login
-	yaml.Unmarshal([]byte(fileContentsPtr), &yamlContents)
+	if err := yaml.Unmarshal([]byte(fileContentsPtr), &yamlContents); err != nil {
+		log.Fatal(err)
+	}
 	// Print out vars
-	fmt.Println("Username to use is: ", yamlContents.Username)
-	fmt.Println("Password to use is: ", yamlContents.Password)
+	//fmt.Println("Username to use is: ", yamlContents.Username)
+	//fmt.Println("Password to use is: ", yamlContents.Password)
+	//fmt.Println("Connecting to", yamlContents.Username+"@"+hostArg, "on port", portArg)
 	fmt.Println("Msg to use is: '", yamlContents.Msg, "'")
+
+	// Create a session config then create a session
+	sessionConfigPtr := &ssh.ClientConfig{
+		User: yamlContents.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(yamlContents.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	fmt.Println("Trying to connect to", yamlContents.Username+"@"+hostArg, "on port", portArg+"...")
+	conn, err := ssh.Dial("tcp", hostArg+":"+portArg, sessionConfigPtr)
+	check(err)
+	defer func() {
+		conn.Close()
+		fmt.Println("Closing connection")
+	}()
+	fmt.Println("Connected to", yamlContents.Username+"@"+hostArg, "on port", portArg+"...")
+
+	// Create a new tty session
+	clientSession, err := conn.NewSession()
+	check(err)
+	defer func() {
+		clientSession.Close()
+		fmt.Println("Closing session")
+	}()
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,
+		ssh.TTY_OP_ISPEED: 115200,
+		ssh.TTY_OP_OSPEED: 115200,
+	}
+
+	if err := clientSession.RequestPty("xterm", 40, 80, modes); err != nil {
+		log.Fatal(err)
+	}
+
+	// Establish stdin and stdout, write msg from file into stdin, run command, and take output from stdout print to screen
+	stdin, err := clientSession.StdinPipe()
+	check(err)
+	stdout, err := clientSession.StdoutPipe()
+	check(err)
+	if err := clientSession.Shell(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Sending msg...")
+	// - Turn the message into bytes for the Write method, also add at end of message actually leaving the host (should probably be defined in a file but this is good for now)
+	msg := []byte(yamlContents.Msg + "\n" + "quit\n")
+	stdin.Write(msg)
+	defer func() {
+		fmt.Println("Command run, output below:")
+		io.Copy(log.Writer(), stdout)
+	}()
 }
